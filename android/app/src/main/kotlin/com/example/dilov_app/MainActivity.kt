73e93d86471ecc35771429e0d5caf230673a556a
supardi_9512 <supardi@atomic.id>
2023-06-24 15:09:34 +0700

    initial project

---
+++ android/app/src/main/kotlin/com/example/dilov_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dilov_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Image.asset('assets/images/hero.png'),
+        ),
+      ),
+    );
+  }
+}
